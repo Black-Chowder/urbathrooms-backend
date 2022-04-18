d6ecfd38a6c9be63580c144c4d40e3d3f36fcfbb
MrManBLC <gman.dude132@gmail.com>
2022-04-18 17:54:50 -0400

    Added get for info portion of API

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }));
+app.use(express.json());
 
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '5000';
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
@@ -17,7 +18,19 @@ app.on("error", err => {
     console.log(err);
 });
 
-app.get('/test', (req, res) => {
-    console.log("Rout Reached");
-    res.send("received!");
+type InfoResp = {
+    description: string,
+    name: string,
+    rating: number
+};
+app.get('/info', (req, res) => {
+    console.log(`Requesting info for ${req.query.locId}`);
+
+    let toReturn: InfoResp = {
+        description: "test description",
+        name: "test name",
+        rating: 4.5
+    }
+    
+    res.status(200).send(toReturn);
 });
